e0d533c36a34b62cdd0d238c00d1649e83bae04c
Ben Millar <91565892+techlobster@users.noreply.github.com>
2025-01-05 10:53:06 -0500

    Create cout-object-example.cpp

---
+++ cout-object-example.cpp
@@ -0,0 +1,20 @@
+// Create a statement that prints two sentences and a number on multiple lines
+
+#include <iostream>
+using namspace std;
+
+// Adding \n within your quotes below creates a new line for each value within these ""
+// We use cout again to switch from words to numbers
+// return 0 ends the statement
+
+int (main) {
+  cout << "This is my first C++ object. \n"
+    "Enjoy your day! \n";
+  cout << 1 + 2;  
+  return 0;
+}
+
+// Output which will be without these // marks:
+// This is my first C++ object.
+// Have a great day!
+// 3
